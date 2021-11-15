da55bbdef2fa5d43f50177ce918ac815154aa6cb
Hsalah9712 <hassansalehh3@gmail.com>
2021-11-16 00:29:01 +0200

    Responsive Website Using By React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import All Components From Index.js File In Containers [Big Components] File
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
+
+// Import All Components From Index.js File In Component [Small Components] File
+import { Brand, CTA, Navbar } from './components';
+
+import './styles/App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
